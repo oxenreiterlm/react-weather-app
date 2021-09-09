f04db787b3530d43fa1f3fa9090c7121323fb1a1
oxenreiterlm <86936353+oxenreiterlm@users.noreply.github.com>
2021-09-09 17:00:02 -0400

    updated layout and adjusted some styling on CurrentWeather/MainSection.css

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -5,38 +5,30 @@ export default function CurrentWeather() {
   return (
     <div>
       <div className="currentLocAndTime">
-        <h2>
-          Currently in <em className="cityUpdate">Pittsburgh</em>:
-        </h2>
+        <h2 className="cityUpdate">Pittsburgh</h2>
         <div className="currentDateTime">
           <h6 id="date-time-now">Updated: 8/29/21 at 8:02</h6>
         </div>
       </div>
-      <div className="row align-items-start">
-        <div className="col-sm-6 ">
+      <div className="row">
+        <div className="col-sm-4">
           <img
             id="current-weather-icon"
-            className="currentIcon img-fluid d-flex"
+            className="currentIcon img-fluid d-flex float-end"
             alt="Current Weather"
             src={sunny}
           />
         </div>
-        <div className="col-sm-6">
-          <h3 className="currentCard">
-            <span id="temp">89</span>
-            <span className="units">
-              °
-              <a href="/" className="active" id="fahrenheit-link">
-                F
-              </a>{" "}
-              | °
-              <a href="/" className="inactive" id="celsius-link">
-                C
-              </a>
-            </span>
-          </h3>
-          <h4>
-            <div id="weather-desc-input">sunny</div>
+        <div className="col-sm-3 align-self-center p-0">
+          <div className="row currentTempWithUnits p-0">
+            <span className="currentTemp p-0">89</span>
+            <span className="currentUnits p-0">°F/°C</span>
+          </div>
+          <div className="row currentDesc">sunny</div>
+        </div>
+        <div className="col-sm-5 p-0 align-self-center">
+          <h4 className="otherCurrentDesc">
+            <div>21% chance of precipitation</div>
             <div id="wind-speed-input">wind speed: 1mph</div>
             <div id="humidity-input">humidity: 90%</div>
           </h4>
